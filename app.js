68e588f0f16457605ace59c02a32abb8ac549516
RomanBednyakov <RomanBednyakov>
2018-06-22 13:14:15 +0300

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const passport = require('./server/passportJs/index').passportJs;
 
-app.use('/login', express.static('view/login'));
-app.use('/registration', express.static('view/registration'));
-app.use('/', express.static('view/home'));
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
