d45f3ca53c507a63e003f8fca77c27f75903be06
RomanBednyakov <RomanBednyakov>
2018-07-02 17:56:47 +0300

    refactoring

--- server/controller/search.js
+++ server/controller/search.js
@@ -2,13 +2,12 @@ const sequelize = require('../sequelize/index');
 
 module.exports = async function (req, res, next) {
     try {
-        console.log('req.headers', req.headers.textSearch);
-        let users = await sequelize.query(`SELECT name, id FROM users WHERE name LIKE :search AND NOT id = :id`, {replacements: {search : `${req.headers.text-search}%`, id: req.body.userId}, type: sequelize.QueryTypes.SELECT});
+        let users = await sequelize.query(`SELECT name, id FROM users WHERE name LIKE :search AND NOT id = :id`, {replacements: {search : `${req.query.value}%`, id: req.body.userId}, type: sequelize.QueryTypes.SELECT});
         if (users.length > 0) {
             let friends = await sequelize.query(`SELECT following FROM followers WHERE follower ='${req.body.userId}'`, {type: sequelize.QueryTypes.SELECT});
             await friends.forEach((item) => {
                 users.forEach((users) => {
-                    if (String(item.following) ===String(users.id)) {
+                    if (String(item.following) === String(users.id)) {
                         users.following = item.following;
                     }
                 });
